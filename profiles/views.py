d5501ab82c24233e658b60b9cad77ba8887e2b7f
mnjammy007 <naseem4954r@gmail.com>
2024-08-09 18:14:08 +0530

    lecture 12/007 completed

--- profiles/views.py
+++ profiles/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
-
-def store_file(file):
-    with open("media/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+from .forms import ProfileForm
+from .models import UserProfile
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {"form": form})
 
     def post(self, request):
-        profile_image = request.FILES["image"]
-        store_file(profile_image)
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(user_image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+        return render(request, "profiles/create_profile.html", {"form": submitted_form})
