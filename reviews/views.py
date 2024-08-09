9f07d1fdbe90e67bf13ac7979a9aa979a1244ab0
mnjammy007 <naseem4954r@gmail.com>
2024-08-09 16:24:52 +0530

    module 11 complete

--- reviews/views.py
+++ reviews/views.py
@@ -5,23 +5,17 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {"form": form})
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, "reviews/review.html", {"form": form})
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 
 class ThankYouView(TemplateView):
@@ -38,19 +32,7 @@ class ReviewsListView(ListView):
     model = Review
     context_object_name = "reviews"
 
-    # def get_queryset(self) -> QuerySet[Any]:
-    #     base_query = super().get_queryset()
-    #     new_query = base_query.filter(rating__gte=3)
-    #     return new_query
-
 
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
-
-    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    #     context = super().get_context_data(**kwargs)
-    #     review_id = kwargs["pk"]
-    #     review = Review.objects.get(pk=review_id)
-    #     context["review"] = review
-    #     return context
