bdc3ebcc4427943613de479dc87dfbbc555b6465
mnjammy007 <naseem4954r@gmail.com>
2024-08-10 13:31:01 +0530

    module 13 completed

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.ReviewView.as_view(), name="review-form"),
     path("thank-you/", views.ThankYouView.as_view(), name="thank-you"),
     path("reviews/", views.ReviewsListView.as_view(), name="reviews"),
+    path("reviews/favorite", views.AddFavoriteView.as_view(), name="favorite"),
     path("reviews/<int:pk>/", views.SingleReviewView.as_view(), name="review-detail"),
 ]
